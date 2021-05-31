4afff17ea6c91b32dd8fab7b71bae93d0357e7f9
h1m <himanshu.yadav643@gmail.com>
2021-06-01 03:58:44 +0530

    transpiling works with esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+  const ref = useRef<any>();
+
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = () => {
-    console.log(input);
+  // use useEffect to call startService only one time
+  useEffect(() => {
+    startService();
+  }, []);
+
+  // initializing to use esbuild-wasm binary
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    // we are transpiling the code written in the text area
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    // setting the state for code here
+    setCode(result.code);
   };
 
   return (
