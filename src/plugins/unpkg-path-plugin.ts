380a2c7bda70b6d21a9c7fa70fa3685a930fc7e7
h1m <himanshu.yadav643@gmail.com>
2021-06-15 01:14:07 +0530

    working on unpkg plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,12 +10,22 @@ export const unpkgPathPlugin = () => {
 
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
             namespace: "a",
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
+
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: "a",
+        };
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -25,15 +35,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require("tiny-test-pkg");
+              const message = require("nested-test-pkg");
               console.log(message);
             `,
           };
         }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responeURL).pathname,
         };
       });
     },
