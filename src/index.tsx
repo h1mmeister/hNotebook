66c1fb23ca48135e3e40cb957adac2f89cd240e7
h1m <himanshu.yadav643@gmail.com>
2021-06-17 18:03:17 +0530

    refactoring the plugin

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,7 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(input)],
       define: {
         "process.env.NODE_ENV": '"productio"',
         global: "window",
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,30 +6,52 @@ const fileCache = localforage.createInstance({
   name: "fileCache",
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
+      // build.onResolve({ filter: /.*/ }, async (args: any) => {
+      //   console.log("onResolve", args);
 
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
+      //   if (args.path === "index.js") {
+      //     return { path: args.path, namespace: "a" };
+      //   }
 
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
+      //   if (args.path.includes("./") || args.path.includes("../")) {
+      //     return {
+      //       namespace: "a",
+      //       path: new URL(
+      //         args.path,
+      //         "https://unpkg.com" + args.resolveDir + "/"
+      //       ).href,
+      //     };
+      //   }
 
+      //   return {
+      //     path: `https://unpkg.com/${args.path}`,
+      //     namespace: "a",
+      //   };
+      // });
+
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          path: `https://unpkg.com/${args.path}`,
           namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
         };
       });
 
@@ -39,10 +61,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return {
             loader: "jsx",
-            contents: `
-              const message = require("react");
-              console.log(message);
-            `,
+            contents: inputCode,
           };
         }
 
