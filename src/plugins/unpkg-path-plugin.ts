aa0abf49d93c18623f37cfa4b2276283f1169717
h1m <himanshu.yadav643@gmail.com>
2021-06-17 17:32:21 +0530

    working on the caching part while downloading files from npm

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 import * as esbuild from "esbuild-wasm";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "fileCache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
